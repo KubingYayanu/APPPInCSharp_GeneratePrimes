48467bdc1f58d091b297d49f52cc4c85f97c37eb
Kubing Lin <edkt973@gmail.com>
2019-03-03 14:22:41 +0800

    add TestPrimes method

--- GeneratePrimes.UnitTests/GeneratePrimesTests.cs
+++ GeneratePrimes.UnitTests/GeneratePrimesTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿
+using NUnit.Framework;
 
 namespace GeneratePrimes.UnitTests
 {
@@ -6,8 +7,23 @@ namespace GeneratePrimes.UnitTests
     public class GeneratePrimesTests
     {
         [Test]
-        public void TestMethod1()
+        public void TestPrimess()
         {
+            int[] nullArray = Console.GeneratePrimes.GeneratePrimeNumbers(0);
+            Assert.That(nullArray.Length, Is.EqualTo(0));
+
+            int[] minArray = Console.GeneratePrimes.GeneratePrimeNumbers(2);
+            Assert.That(minArray.Length, Is.EqualTo(1));
+            Assert.That(minArray[0], Is.EqualTo(2));
+
+            int[] threeArray = Console.GeneratePrimes.GeneratePrimeNumbers(3);
+            Assert.That(threeArray.Length, Is.EqualTo(2));
+            Assert.That(threeArray[0], Is.EqualTo(2));
+            Assert.That(threeArray[1], Is.EqualTo(3));
+
+            int[] centArray = Console.GeneratePrimes.GeneratePrimeNumbers(100);
+            Assert.That(centArray.Length, Is.EqualTo(25));
+            Assert.That(centArray[24], Is.EqualTo(97));
         }
     }
 }
